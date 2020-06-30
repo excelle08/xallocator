3b2f43eec51becbc6f4dde3b712f7c97190472eb
Wei Su <suwei779@gmail.com>
2020-06-30 16:15:42 -0400

    Add data structures for the new allocation scheme

--- include/xallocator.h
+++ include/xallocator.h
@@ -3,16 +3,49 @@
 
 #include <stddef.h>
 #include "DataTypes.h"
+#include "Allocator.h"
+#include "util.h"
 
 // See http://www.codeproject.com/Articles/1084801/Replace-malloc-free-with-a-Fast-Fixed-Block-Memory
 
 #ifdef __cplusplus
+
+struct PoolListNode {
+    Allocator *allocator;
+    PoolListNode *next;
+};
+
+struct PoolListHeader {
+    size_t capacity;
+    PoolListNode *next;
+};
+
+struct XAllocState {
+    /* Primary pool allocator, block size is set to the largest */
+    Allocator primary;
+    /* A pool of secondary allocators
+     * The max number of secondary allocators is equal to the number of total
+     * blocks in the primary allocator */
+    Allocator secondaries;
+    /* A pool of secondary allocator nodes
+     * The max number of nodes is also equal to the number of blocks in the primary
+     * allocator. */
+    Allocator secondary_nodes;
+    /* Headers of secondary allocator lists
+     * This is an array of PoolListHeader objects created during xalloc_init().
+     * Each PoolListHeader element connects a list of secondary pool nodes. Each
+     * node represents a Secondary allocator whose block size = capacity.
+     */
+    size_t n_headers;
+    PoolListHeader *secondary_headers;
+};
+
 // Define AUTOMATIC_XALLOCATOR_INIT_DESTROY to automatically call xalloc_init() and
 // xalloc_destroy() when using xallocator in C++ projects. On embedded systems that
 // never exit, you can save 1-byte of RAM storage per translation unit by undefining
 // AUTOMATIC_XALLOCATOR_INIT_DESTROY and calling xalloc_init() manually before the OS
 // starts. 
-#define AUTOMATIC_XALLOCATOR_INIT_DESTROY 
+// #define AUTOMATIC_XALLOCATOR_INIT_DESTROY 
 #ifdef AUTOMATIC_XALLOCATOR_INIT_DESTROY
 /// If a C++ translation unit, create a static instance of XallocInitDestroy. Any C++
 /// file including xallocator.h will have the xallocDestroy instance declared first
@@ -36,6 +69,42 @@ static XallocInitDestroy xallocInitDestroy;
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/* calc_required_memsize: Calculate total amounts of memory needed based on
+ *   proposed data capacity and block size. Block size must be a power of 2
+ *   and capacity must be a multiple of block size.
+ * 
+ * @param[in] capacity:     Total data capacity
+ * @param[in] blocksize:    The biggest block size
+ * 
+ * @return: The total amount of memory required for this allocator.
+ *          If it returns 0 then it decides that the parameters are invalid.
+ */
+static inline size_t calc_required_memsize(size_t capacity, size_t blocksize)
+{
+    // Check if blocksize is a power of 2
+    if ((blocksize & (blocksize - 1)) != 0)
+        return 0;
+
+    // Check if capacity is a multiple of blocksize
+    if (capacity % blocksize != 0)
+        return 0;
+
+    size_t total = capacity;
+    size_t nblocks = capacity / blocksize;
+    /* Space for the primary allocator itself and the pool of secondaries */
+    total += sizeof(Allocator) * 2;
+    /* Space for potential secondary allocators */
+    total += sizeof(Allocator) * nblocks;
+    /* Space for secondary list nodes */
+    total += sizeof(PoolListNode) * nblocks;
+    /* Space for secondary list headers 
+     * We need log_2(blocksize / 8) headers
+     */
+    total += sizeof(PoolListHeader) * (BITS_TO_REPRESENT(blocksize) - 3);
+
+    return total;
+}
 
 /// This function must be called exactly one time before the operating system
 /// threading starts. If using xallocator exclusively in C files within your application
